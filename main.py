d7c9dc92ae34834f7e7a090ea577226bb08eb8c4
Jan Wasilewski <janwasilewski144@op.pl>
2020-06-07 15:43:15 +0200

    user input

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 import data_set
 import id3
+import numpy as np
 
 data, labels = data_set.load_data("data\\agaricus-lepiota.data")
 
+#tree display prompt
+display_trees = input("Display trees? (y/n): ")
+id3_type = input("Algorytm zwykły czy z ruletką? (regular/roulette): ")
+
 # data_set.display_data(data, 10, [i for i in range(23)])
+f = 0.1
+f = float(input("Podaj rozmiar zbioru treningowego (float): "))
 
-training_data_length = int(len(data) * 0.75)    # 75% of data is training data
-training_data = data[:training_data_length, 1:]
-training_labels = data[:training_data_length, 0]
+training_data_length = int(len(data) * f)    # 0.5% of data is training data
 
-id3_tree = id3.build_tree(training_labels, training_data, variation="roulette")
 
-# tree_labels = data_set.mushrooms_labels()
-tree_labels = None
+k = 1;     #numer of repeats
+k = int(input("Podaj ilość testów (int): "))
 
-id3_tree.print(tree_labels)
+while(k>0):
 
-validation_data = data[training_data_length:, :]
-if len(validation_data) == 0:
-    validation_data = data[:, :]
+    #randomize data order
+    np.random.shuffle(data)
 
-correct_cases = 0
-all_cases = 0
-for validation_case in validation_data:
-    if validation_case[0] == id3_tree.classify(validation_case[1:]):
-        correct_cases += 1
-    all_cases += 1
+    training_data = data[:training_data_length, 1:]
+    training_labels = data[:training_data_length, 0]
 
-print("Accuracy over {} cases: {:.2f}%".format(all_cases, correct_cases / all_cases * 100))
+    id3_tree = id3.build_tree(training_labels, training_data, variation=id3_type) #roulette or regular
+
+    if (display_trees == "y"):
+        tree_labels = data_set.mushrooms_labels()
+        id3_tree.print(tree_labels)
+
+    validation_data = data[training_data_length:, :]
+    if len(validation_data) == 0:
+        validation_data = data[:, :]
+
+    correct_cases = 0
+    all_cases = 0
+    for validation_case in validation_data:
+        if validation_case[0] == id3_tree.classify(validation_case[1:]):
+            correct_cases += 1
+        all_cases += 1
+
+    print("Accuracy over {} cases: {:.2f}%".format(all_cases, correct_cases / all_cases * 100))
+
+    k = k -1
